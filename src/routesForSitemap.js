09413c6991f532f366633c07ff5970fb668f58f4
Enar <enmust@ttu.ee>
2020-05-21 12:41:28 +0300

    added sitemap

---
+++ src/routesForSitemap.js
@@ -0,0 +1,51 @@
+const axios = require("axios");
+
+module.exports = [
+  {
+    path: "*",
+    redirect: "/"
+  },
+  {
+    path: "/",
+    name: "dois",
+    component: () => import("./views/Dois")
+  },
+  {
+    path: "/10.15152/GEO.:id",
+    name: "doi",
+    component: () => import("./components/doi/Doi.vue"),
+    meta: {
+      sitemap: {
+        slugs: async () => await getIdsForSitemap("doi")
+      }
+    }
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: () => import("./views/About")
+  }
+];
+
+async function getIdsForSitemap(table) {
+  if (table) {
+    const API_URL = "https://api.geocollections.info/";
+
+    let url = API_URL + table + "?";
+
+    // if (table === "area") {
+    //   url += "area_type=2&";
+    // } else if (table === "site") {
+    //   url += "project=20&";
+    // } else if (table === "sample") {
+    //   url += "site__project=20&";
+    // }
+
+    url += "fields=id&format=json";
+
+    const response = await axios.get(url);
+
+    if (response.status === 200) return response.data.results || [2];
+    else return [1];
+  } else return [1];
+}
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,15 @@
+const routes = require("./src/routesForSitemap");
+
 module.exports = {
   // Disable source viewing in production
   productionSourceMap: false,
   // EDGE doesn't work otherwise
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+  pluginOptions: {
+    sitemap: {
+      pretty: true,
+      baseURL: "https://doi.geocollections.info",
+      routes
+    }
+  }
 };
